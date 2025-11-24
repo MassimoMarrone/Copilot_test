6fab80940ea5a9e37ec8567a0d6e82440732a2fb
max <marrone549@gmail.com>
2025-11-24 01:42:58 +0100

    style: use cleaner map tiles (CartoDB Positron)

--- src-react/components/ServiceMap.tsx
+++ src-react/components/ServiceMap.tsx
@@ -74,8 +74,8 @@ const ServiceMap: React.FC<ServiceMapProps> = ({ services, onBook }) => {
         style={{ height: "100%", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
         />
         
         <MapUpdater services={servicesWithLocation} />
